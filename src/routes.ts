937981bb3bd90dcfe55d4e8ac0754ed8f76cc571
William Gonçalves <euwilliamgoncalves@gmail.com>
2021-08-01 07:53:19 -0300

    ✨ add authentication route

--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,17 @@
 import { Router } from 'express';
-import { CreateUserController } from './controllers/CreateUserController';
+import { AuthenticateUserController } from './controllers/AuthenticateUserController';
 import { CreateTagController } from './controllers/CreateTagController';
+import { CreateUserController } from './controllers/CreateUserController';
 import { confirmAuthorization } from './middlewares/confirmAuthorization';
 
 const router = Router();
 
+const authenticateUserController = new AuthenticateUserController();
 const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
 
-router.post('/users', createUserController.handle);
+router.post('/login', authenticateUserController.handle);
 router.post('/tags', confirmAuthorization, createTagController.handle);
+router.post('/users', createUserController.handle);
 
 export { router };
