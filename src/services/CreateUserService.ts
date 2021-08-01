a87f99ed8b7e3d51377e5a30873d3035e9d4a463
William Gonçalves <euwilliamgoncalves@gmail.com>
2021-08-01 07:51:26 -0300

    ♻️ refactor CreateUserService to include password

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,20 +1,23 @@
 import { getCustomRepository } from 'typeorm';
 import { UsersRepository } from '../database/repositories/UsersRepository';
+import { hash } from 'bcryptjs';
 
 interface IUserRequest {
   name: string;
   email: string;
   admin?: boolean;
+  password: string;
 }
 
 class CreateUserService {
 
-  async execute({ name, email, admin }: IUserRequest) {
+  async execute({ name, email, admin, password }: IUserRequest) {
     const usersRepository = getCustomRepository(UsersRepository);
     if (!email) { throw new Error('Invalid email'); }
     const userAlreadyExists = await usersRepository.findOne({ email });
     if (userAlreadyExists) { throw new Error('User already exists'); }
-    const user = usersRepository.create({ name, email, admin });
+    const passwordHash = await hash(password, 8);
+    const user = usersRepository.create({ name, email, admin, password: passwordHash });
     await usersRepository.save(user);
     return user;
   }
