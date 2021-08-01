a31b56de89408cfc5ef05f38fb85620f0e275c81
William Gonçalves <euwilliamgoncalves@gmail.com>
2021-07-31 23:23:59 -0300

    🎨 fix string quotes

--- src/database/entities/User.ts
+++ src/database/entities/User.ts
@@ -1,7 +1,7 @@
 import { Entity, PrimaryColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
 import { v4 as uuid } from 'uuid';
 
-@Entity("users")
+@Entity('users')
 class User {
   @PrimaryColumn()
   readonly id: string;
--- src/database/migrations/1627771070973-CreateUsers.ts
+++ src/database/migrations/1627771070973-CreateUsers.ts
@@ -1,39 +1,39 @@
-import { MigrationInterface, QueryRunner, Table } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class CreateUsers1627771070973 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: "users",
+        name: 'users',
         columns: [
           {
-            name: "id",
-            type: "uuid",
+            name: 'id',
+            type: 'uuid',
             isPrimary: true,
           },
           {
-            name: "name",
-            type: "varchar",
+            name: 'name',
+            type: 'varchar',
           },
           {
-            name: "email",
-            type: "varchar",
+            name: 'email',
+            type: 'varchar',
           },
           {
-            name: "admin",
-            type: "boolean",
+            name: 'admin',
+            type: 'boolean',
             default: false,
           },
           {
-            name: "created_at",
-            type: "timestamp",
-            default: "now()",
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
           },
           {
-            name: "updated_at",
-            type: "timestamp",
-            default: "now()",
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
           }
         ]
       })
@@ -41,7 +41,7 @@ export class CreateUsers1627771070973 implements MigrationInterface {
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable("users");
+    await queryRunner.dropTable('users');
   }
 
 }
