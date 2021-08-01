d56ef95dcc8dc2a5e4797d4b8be8265baad894b2
William Gonçalves <euwilliamgoncalves@gmail.com>
2021-08-01 00:06:47 -0300

    ✨ add CreateTag service

---
+++ src/services/CreateTagService.ts
@@ -0,0 +1,18 @@
+import { getCustomRepository } from 'typeorm';
+import { TagsRepository } from '../database/repositories/TagsRepository';
+
+class CreateTagService {
+
+  async execute(name: string) {
+    const tagsRepository = getCustomRepository(TagsRepository);
+    if (!name) { throw new Error('Tag name is required'); }
+    const tagAlreadyExists = await tagsRepository.findOne({ name });
+    if (tagAlreadyExists) { throw new Error('Tag already exists'); }
+    const tag = tagsRepository.create({ name });
+    await tagsRepository.save(tag);
+    return tag;
+  }
+
+}
+
+export { CreateTagService };
