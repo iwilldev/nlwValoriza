00864dddb408bc1f57be8a1cbdaee073d55ebcf4
William Gonçalves <euwilliamgoncalves@gmail.com>
2021-07-31 23:19:39 -0300

    ✨ add error handling middleware

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 import { router } from './routes';
 
 import './database';
@@ -8,4 +9,15 @@ const app = express();
 
 app.use(express.json());
 app.use(router);
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    return res.status(400).json({
+      error: err.message
+    });
+  }
+  return res.status(500).json({
+    status: 'error',
+    error: 'Internal server error'
+  });
+});
 app.listen(3000, () => console.log('Listening on port 3000'));
