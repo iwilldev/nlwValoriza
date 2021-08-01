c169e83e05f81f48c85bc7b83d55f46503d32d42
William Gonçalves <euwilliamgoncalves@gmail.com>
2021-08-01 00:08:32 -0300

    ✨ update routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 import { CreateUserController } from './controllers/CreateUserController';
+import { CreateTagController } from './controllers/CreateTagController';
+import { confirmAuthorization } from './middlewares/confirmAuthorization';
 
 const router = Router();
 
 const createUserController = new CreateUserController();
+const createTagController = new CreateTagController();
 
 router.post('/users', createUserController.handle);
+router.post('/tags', confirmAuthorization, createTagController.handle);
 
 export { router };
