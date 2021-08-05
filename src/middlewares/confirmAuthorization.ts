b23890efcc2870e19b0803115f5abbc112d8f0f7
William Gonçalves <euwilliamgoncalves@gmail.com>
2021-08-05 09:51:00 -0300

    ♻️ refactor confirmAuthorization middleware

--- src/middlewares/confirmAuthorization.ts
+++ src/middlewares/confirmAuthorization.ts
@@ -1,7 +1,11 @@
 import { Request, Response, NextFunction } from 'express';
+import { getCustomRepository } from 'typeorm';
+import { UsersRepository } from '../database/repositories/UsersRepository';
 
-export function confirmAuthorization(req: Request, res: Response, next: NextFunction) {
-  const admin = true;
+export async function confirmAuthorization(req: Request, res: Response, next: NextFunction) {
+  const { user_id } = req;
+  const usersRepository = getCustomRepository(UsersRepository);
+  const { admin } = await usersRepository.findOne(user_id);
   if (admin) { return next(); }
-  return res.status(401).json({ error: 'Unauthorized' });
+  return res.status(403).json({ error: 'Forbidden' });
 }
